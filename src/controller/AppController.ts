ff27aef4c04d49b94072caee1ffeb2f79f1db545
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-09-15 19:36:26 -0300

    integracao com gemini ok

--- src/controller/AppController.ts
+++ src/controller/AppController.ts
@@ -1,7 +1,53 @@
 import { Request, Response } from "express";
+import { GoogleAIFileManager } from '@google/generative-ai/server';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 export class AppController {
-    async upload(_req: Request, res: Response) {
-        return res.json("Estou na rota post")
+    async upload(req: Request, res: Response) {
+
+
+
+        if (!process.env.GEMINI_API_KEY) {
+            return res.status(401).json({ message: 'GEMINI_API_KEY is required' });
+        }
+
+        const fileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY);
+
+        if (req.file) {
+            const { mimetype, originalname, path } = req.file;
+
+            const uploadResonse = await fileManager.uploadFile(path, {
+                mimeType: mimetype,
+                displayName: originalname
+            })
+
+            const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+            const model = genAi.getGenerativeModel(
+                {
+                    model: 'gemini-1.5-pro',
+                }
+            )
+
+            const result = await model.generateContent([
+                {
+                    fileData: {
+                        mimeType: uploadResonse.file.mimeType,
+                        fileUri: uploadResonse.file.uri
+                    }
+                },
+                {
+                    text: 'qual a quantidade de kWh consumido neste medidor?, retorne a resposta do medidor sempre entre asteriscos **'
+                }
+            ]);
+
+            console.log(result.response.text());
+        }
+
+
+
+
+
+        return res.json("Estou na rota post");
     }
 }
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { AppController } from '../controller/AppController';
+import multer from '../config/multer';
+
 
 const route = Router();
 
 const appRoute = new AppController();
 
-route.post('/upload', appRoute.upload);
+route.post('/upload', multer.single('image'), appRoute.upload);
 
 export default route;
